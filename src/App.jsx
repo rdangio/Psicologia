ad86d1b3a313f3419df8f79d768e72093b7846bd
Rico <ricojau@hotmail.com>
2024-09-27 16:34:09 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Nav from "./components/Nav"
+
+const App = () => {
+  return (
+      <div className="bg-slate-900">
+        <Nav />
+      </div>
+  )
+
+}
+
+export default App
---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+
+
+const Logo = () => {
+  return (
+    <div>
+      <img className="size-20 ali  rounded-[20%]" src={'/src/assets/logo.jpg'} alt="logo" />
+    </div>
+  );
+}
+
+export default Logo;
+
