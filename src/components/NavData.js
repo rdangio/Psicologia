a0659b4bffef58959c200fb728108845287b3c75
deivideguilherme <deivide.jau@gmail.com>
2024-09-28 15:43:53 -0300

    Refeito menu utilizando o '.map', criado componentes .js com as informações pertinentes ao menu

---
+++ src/components/NavData.js
@@ -0,0 +1,41 @@
+export const NavData = [
+  {
+    title: "Home",
+    to: "Home",
+    url: "/",
+    cName:
+      "hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer",
+  },
+
+  {
+    title: "Sobre",
+    to: "About",
+    url: "#",
+    cName:
+      "hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer",
+  },
+
+  {
+    title: "Serviços",
+    to: "Services",
+    url: "#",
+    cName:
+      "hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer",
+  },
+
+  {
+    title: "Projetos",
+    to: "Project",
+    url: "#",
+    cName:
+      "hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer",
+  },
+
+  {
+    title: "Contato",
+    to: "Contact",
+    url: "#",
+    cName:
+      "hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer",
+  },
+];
---
+++ src/components/NavDataResponsive.js
@@ -0,0 +1,36 @@
+export const NavDataResponsive = [
+  {
+    title: "Home",
+    to: "Home",
+    url: "/",
+    cName: "my-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded",
+  },
+
+  {
+    title: "Sobre",
+    to: "About",
+    url: "#",
+    cName: "my-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded",
+  },
+
+  {
+    title: "Serviços",
+    to: "Services",
+    url: "#",
+    cName: "my-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded",
+  },
+
+  {
+    title: "Projetos",
+    to: "Project",
+    url: "#",
+    cName: "my-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded",
+  },
+
+  {
+    title: "Contato",
+    to: "Contact",
+    url: "#",
+    cName: "my-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded",
+  },
+];
