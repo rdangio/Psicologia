bd3f784a5b99e31d40adad97664cafe802a4bfba
deivideguilherme <deivide.jau@gmail.com>
2024-09-28 11:25:35 -0300

    Adicionado comentário da onde se encontra o logo, e ajustado o texto do menu ao centro com o align-items

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -46,12 +46,15 @@ const Nav = () => {
   return (
     <nav>
       <div className="h-10vh flex justify-between z-50 text-white lg:py-5 px-20 py-4">
+        {/* Logo -------> */}
         <div className="flex items-center flex-1">
-          <span className="text-3xl font-bold">
+          <span>
             <Logo />
           </span>
         </div>
-        <div className="lg:flex md:flex lg: flex-1 items center justify-end font-normal hidden">
+        {/* Logo <------- */}
+
+        <div className="lg:flex md:flex lg: flex-1 items center justify-end font-normal hidden items-center">
           <div className="flex-10">
             <ul className="flex gap-8 mr-16 text-[18px]">
               <Link spy={true} smooth={true} to="Home">
