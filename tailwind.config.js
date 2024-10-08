db0b63cf4fa9803d44c3e68b17c9736d4c8ed911
deivideguilherme <deivide.jau@gmail.com>
2024-10-08 11:04:26 -0300

    Ajustado arquivo tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,13 +16,8 @@ export default {
 
         //Botões
         "button-color": "#17252A",
-<<<<<<< HEAD
-        "button-hover": "#2B7A78",
-        "button-active": "#44BDBB",
-=======
         "button-hover": "#44BDBB",
         "button-active": "#2B7A78",
->>>>>>> dvd
         "titlecolor-1": "#17252A",
       },
     },
