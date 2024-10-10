e4bbc69df196d04fd6c6981bfa4fd2e0ce7b6b88
deivideguilherme <deivide.jau@gmail.com>
2024-10-10 17:13:57 -0300

    Definição de cores para projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        // Cores ícone whatsapp
+        //Whatsapp
         "whats-green": "#25d366",
         "whats-green-hover": "#0fe961",
         "whats-green-active": "#36c26a",
@@ -12,11 +12,22 @@ export default {
         //Cores do projeto
         background: "#2B7A78",
         nav: "#17252A",
-        button: "#17252A",
-        hover: "#",
-        active: "#",
+
+        //Botão 1
+        button1: "#17252A",
+        hover1: "#2B7A78",
+        active1: "#3AAFA9",
+
+        //Botão 2
+        button2: "#",
+        hover2: "#",
+        active2: "#",
+
+        //Títulos
         title1: "#17252A",
         title2: "#DEF2F1",
+
+        //Textos
         text1: "#17252A",
         text2: "#FEFFFF",
       },
