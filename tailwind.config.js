959d88f7007198a740fce0457f365f2a9fe4e322
deivideguilherme <deivide.jau@gmail.com>
2024-10-11 22:12:56 -0300

    Criado novo padrão para cores e tamanhos do projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,39 +3,47 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-
       colors: {
         //Whatsapp
-        "whats-green": "#25d366",
-        "whats-green-hover": "#0fe961",
-        "whats-green-active": "#36c26a",
+        "c-whats": "#25d366",
+        "c-whats-hover": "#0fe961",
+        "c-whats-active": "#36c26a",
 
         //Cores do projeto
-        background: "#2B7A78",
-        nav: "#17252A",
+        "c-background": "#2B7A78",
+        "c-nav": "#17252A",
 
         //Botão 1
-        button1: "#17252A",
-        hover1: "#2B7A78",
-        active1: "#3AAFA9",
+        "c-button1": "#17252A",
+        "c-hover1": "#2B7A78",
+        "c-active1": "#3AAFA9",
 
         //Botão 2
-        button2: "#",
-        hover2: "#",
-        active2: "#",
+        "c-button2": "#",
+        "c-hover2": "#",
+        "c-active2": "#",
 
         //Títulos
-        title1: "#17252A",
-        title2: "#DEF2F1",
+        "c-title1": "#17252A",
+        "c-title2": "#DEF2F1",
 
         //Textos
-        text1: "#17252A",
-        text2: "#FEFFFF",
+        "c-text1": "#17252A",
+        "c-text2": "#FEFFFF",
       },
 
       fontFamily: {
-        roboto: ['Roboto', 'sans-serif'],
-        'roboto-bold': ['Roboto-Bold', 'sans-serif']
+        roboto: ["Roboto", "sans-serif"],
+        "roboto-bold": ["Roboto-Bold", "sans-serif"],
+      },
+
+      fontSize: {
+        "fs-nav": "1rem",
+        "fs-h1": "2.5rem",
+        "fs-h2": "2rem",
+        "fs-text": "1rem",
+        "fs-button": "0.875rem",
+        "fs-footer": "0.75rem",
       },
     },
   },
