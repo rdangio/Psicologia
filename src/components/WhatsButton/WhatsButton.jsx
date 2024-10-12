5573950b81c80755567f926204a0d398963171a8
deivideguilherme <deivide.jau@gmail.com>
2024-10-11 22:32:17 -0300

    Alterações do padrão de cores do botão whatsapp

--- src/components/WhatsButton/WhatsButton.jsx
+++ src/components/WhatsButton/WhatsButton.jsx
@@ -3,13 +3,13 @@ const Whatsapp = () => {
     <div className="">
       <a
         href="https://api.whatsapp.com/send?phone=5514996923650&text=Gostaria%20de%20ajuda?%20Vamos%20Convesar!"
-        className="fixed cursor-pointer w-14 h-14 bottom-10 right-6 bg-whats-green hover:bg-whats-green-hover active:bg-whats-green-active ease-in-out transition-all text-white rounded-full text-center text-4xl shadow-lg z-50"
+        className="fixed cursor-pointer w-14 h-14 bottom-10 right-6 bg-c-whats hover:bg-c-whats-hover active:bg-c-whats-active ease-in-out transition-all text-c-text2 rounded-full text-center text-4xl shadow-lg z-50"
         rel="noreferrer"
         target="_blank"
       >
         <img
           src={"/src/assets/Logos/whatsapp.svg"}
-          className="mt-2 ml-[10px] text-white w-9"
+          className="mt-2 ml-[10px] text-c-text2 w-9"
         />
       </a>
     </div>
