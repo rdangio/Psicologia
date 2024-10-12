9bc14e3155efc1e98ed22075ef7012166b158bee
deivideguilherme <deivide.jau@gmail.com>
2024-10-11 22:11:33 -0300

    Alterado o nome das cores e padrão de tamanho mediante o arquivo tailwind.config.js

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -16,7 +16,7 @@ const Nav = () => {
 
   const content = (
     // Menu responsivo ------->
-    <div className="lg:hidden block absolute top-16 w-full left-0 right-0 bg-slate-600 transition">
+    <div className="lg:hidden block absolute top-14 w-full left-0 right-0 bg-c-nav transition">
       <ul className="text-center text-xl p-20">
         {NavDataResponsive.map((item, index) => (
           <Link key={index} spy={true} smooth={true} to={item.to}>
@@ -30,17 +30,17 @@ const Nav = () => {
 
   return (
     <nav className="flex justify-center">
-      <div className="bg-nav fixed rounded-lg shadow-2xl w-[85%] h-20 flex z-50 text-text2 lg:py-5 px-20 py-4">
+      <div className="top-3 bg-c-nav fixed rounded-lg shadow-2xl w-[85%] h-24 flex z-50 text-c-text2 lg:py-5 px-20 py-4 text-fs-button">
         {/* Logo -------> */}
-        <nav className="flex items-center flex-1 z-30">
-          <span>
-            <Logo />
-          </span>
-        </nav>
+        <div className="flex items-center flex-1 z-30">
+          <Logo />
+        </div>
         {/* Logo <------- */}
-        <div className="lg:flex md:flex lg:flex-1 items center justify-end font-normal hidden items-center z-10">
-          <div className="lg:flex md:flex lg:flex-1 items-center justify-end font-normal hidden">
-            <ul className="flex gap-8 mr-16 text-[18px]">
+
+        {/* Menu -------> */}
+        <div className="lg:flex md:flex lg:flex-1 items center justify-end hidden items-center z-10">
+          <div className="lg:flex md:flex lg:flex-1 items-center justify-end hidden">
+            <ul className="flex gap-8 mr-16">
               {NavData.map((item, index) => (
                 <Link key={index} spy={true} smooth={true} to={item.to}>
                   <li className={item.cName}>{item.title}</li>
@@ -50,10 +50,14 @@ const Nav = () => {
             <DefaultButton>Agende sua Sessão</DefaultButton>
           </div>
         </div>
+        {/* Menu <------- */}
+
+        {/* Menu responsivo recolhido -------> */}
         <div>{click && content}</div>
         <button className="block sm:hidden transition" onClick={handleClick}>
           {click ? <FaTimes /> : <CiMenuFries />}
         </button>
+        {/* Menu responsivo recolhido <------- */}
       </div>
     </nav>
   );
