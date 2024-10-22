0d548d43d9a02683497ba234d84aa9944147bf95
deivideguilherme <deivide.jau@gmail.com>
2024-10-21 22:58:55 -0300

    Início das modificações do footer

--- src/components/Footer/FooterData.js
+++ src/components/Footer/FooterData.js
@@ -1,44 +1,41 @@
-import { FaFacebook, FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
+
+const defaultCname =
+  "text-[22px] list-none font-semibold text-pink-500 py-2 uppercase";
+
+const defaultSocialLinks = "text-gray-500 cursor-pointer hover:text-gray-700";
 
 export const FooterData = [
   {
-    title: "Creativity",
-    items: ["Website Guidline & Ideas", "Tips & Tricks", "Photography"],
-    cName: "text-[22px] list-none font-semibold text-pink-500 py-2 uppercase",
+    title: "Cláudia Dangió",
+    items: ["Psicóloga"],
+    subItems: ["CRP 06/136891"],
+    cName: "mt-2 text-sm text-gray-500",
   },
   {
     title: "Guidline & Ideas",
     items: ["Guidline & Ideas", "Tips & Tricks", "Photography"],
-    cName: "text-[22px] list-none font-semibold text-pink-500 py-2 uppercase",
+    cName: defaultCname,
   },
   {
     title: "Contato",
     items: ["E-mail: seu-email@email.com", "Telefone: +55 14 99999-8888"],
-    cName: "text-[22px] list-none font-semibold text-pink-500 py-2 uppercase",
+    cName: defaultCname,
     socialLinks: [
       {
-        icon: FaGithub,
-        href: "https://www.github.com",
-        cName:
-          "text-white hover:text-pink-500 transform hover:scale-150 transition-all duration-150 ease-in-out ",
-      },
-      {
         icon: FaLinkedin,
-        href: "https://www.linkedin.com",
-        cName:
-          "text-white hover:text-pink-500 transform hover:scale-150 transition-all duration-150 ease-in-out ",
+        href: "https://www.linkedin.com/in/claudia-dangi%C3%B3-21100469/",
+        cName: defaultSocialLinks,
       },
       {
         icon: FaInstagram,
-        href: "https://www.instagram.com",
-        cName:
-          "text-white hover:text-pink-500 transform hover:scale-150 transition-all duration-150 ease-in-out ",
+        href: "https://www.instagram.com/claudia_dangio/",
+        cName: defaultSocialLinks,
       },
       {
         icon: FaFacebook,
-        href: "https://www.facebook.com",
-        cName:
-          "text-white hover:text-pink-500 transform hover:scale-150 transition-all duration-150 ease-in-out ",
+        href: "https://www.facebook.com/claudia.dangio",
+        cName: defaultSocialLinks,
       },
     ],
   },
