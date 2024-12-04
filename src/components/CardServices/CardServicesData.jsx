6490b9f877a4571db4ad280aeedeb1581e526863
Rico <ricojau@hotmail.com>
2024-12-04 08:54:49 -0300

    teste vite2

--- src/components/CardServices/CardServicesData.jsx
+++ src/components/CardServices/CardServicesData.jsx
@@ -1,19 +1,19 @@
 export const dataCardServices = [
   {
     id: "1",
-    image: "/src/assets/Images/service-1.jpg",
+    image: "/assets/Images/service-1.jpg",
     title: "Ansiedade",
     text: "A preocupação excessiva, os pensamentos acelerados e repetitivos...",
   },
   {
     id: "2",
-    image: "/src/assets/Images/service-2.jpg",
+    image: "/assets/Images/service-2.jpg",
     title: "Relacionamento",
     text: "Discussões e desentendimentos frequentes, falta de contato físico, poucos momentos alegres...",
   },
   {
     id: "3",
-    image: "/src/assets/Images/service-3.jpg",
+    image: "/assets/Images/service-3.jpg",
     title: "Depressão",
     text: "Alterações no humor, voltadas principalmente aos sentimentos de tristeza...",
   },
