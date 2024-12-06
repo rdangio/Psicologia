6bfdf24813b2e79528478df5071dc8b9a7e14c07
Rico <ricojau@hotmail.com>
2024-12-06 13:48:38 -0300

    teste largura em tela pequena

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -13,8 +13,8 @@ const Home = () => {
   return (
     <>
       <GlobalStyle />
-      {/* <Nav />
-      <Carousel /> */}
+      <Nav />
+      <Carousel />
       <CardServices />
       <Banner />
       <CardValues />
