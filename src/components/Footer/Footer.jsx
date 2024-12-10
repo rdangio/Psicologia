b83286a81f1e8f83a90f8b23b6333dc0fd814b88
Rico <ricojau@hotmail.com>
2024-12-10 10:27:24 -0300

    reverter footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,36 +1,46 @@
+import Form from "../Form";
 import Logo from "../Logo/Logo";
 import { FooterData } from "./FooterData";
-import { Description } from "../Description/Description";
 
 const Footer = () => {
   //Const para atualizar o ano junto ao rodapé
   const Year = new Date().getFullYear();
 
   return (
-    <footer id="contato" className="w-[100%] bg-c-turquoise-5">
-      <div className="flex flex-col items-center justify-between pt-8">
-        <Logo showSubtitle={true} showSocialMedia={false} />
-        <div className="w-[100%]">
-          <div className="mt-4 flex flex-col justify-center text-center md:mt-6 md:justify-end lg:mt-5 ">
-            <Description className="text-center text-c-turquoise-1">
-              Como me encontrar
-            </Description>
-            <ul className="py-3 list-none justify-center">
+    <footer id="contato"  className="w-full bg-c-turquoise-5">
+      <div className="md:flex-no-wrap container mx-auto flex flex-col flex-wrap justify-between px-10 py-10 sm:items-center md:flex-row md:items-center md:justify-around lg:items-start">
+        <Logo showSubtitle={true} showSocialMedia={true} />
+
+        <div className="xs:w-60">
+          <div className="mt-10 flex flex-grow flex-col text-center md:mt-10 md:justify-end md:text-left lg:mt-5 lg:justify-center">
+            <h2 className="title-font text-sm font-medium uppercase tracking-widest text-c-turquoise-1">
+              Sobre
+            </h2>
+            <ul className="mt-3 list-none">
+              <li className="mb-3">
+                <a
+                  href="#"
+                  className="cursor-pointer text-gray-400 hover:text-c-turquoise-4"
+                >
+                  Quem Sou?
+                </a>
+              </li>
               {FooterData.map((item, index) => (
                 <li key={index} className={item.cName}>
-                  <span className="flex gap-2">
-                    {item.icon} {item.icon2}
-                  </span>
                   {item.title}
                 </li>
               ))}
             </ul>
           </div>
         </div>
+
+        <div className="flex w-96 items-center text-center sm:mt-10 md:mt-5">
+          {/* Formulário */}
+          <Form />
+        </div>
       </div>
-      
       <div className="w-full bg-gray-300">
-        <div className="container mx-auto px-5 py-2">
+        <div className="container mx-auto px-5 py-4">
           <p className="text-sm capitalize text-c-turquoise-5 xs:text-center">
             &copy; {Year} RD Dev&Design
           </p>
--- src/components/Footer/FooterData.jsx.new
+++ src/components/Footer/FooterData.jsx.new
@@ -1,26 +1,40 @@
 import LinkedInIcon from "../Icons/LinkedInIcon";
 import FacebookIcon from "../Icons/FacebookIcon";
 import InstagramIcon from "../Icons/InstagramIcon";
+import LocationIcon from "../Icons/Location";
+import EmailIcon from "../Icons/Email";
+import PhoneIcon from "../Icons/Phone";
+import WhatsappIcon from "../Icons/Whatsapp";
 
-const defaultCname = "mb-3 text-gray-400";
+const defaultCname =
+  "mb-3 text-c-turquoise-1 text-center flex flex-col items-center font-roboto text-fs-h7 sm:text-fs-h6 md:text-fs-h6 lg:text-fs-h6 xl:text-fs-h4";
 
 const defaultSocialLinks = "text-c-turquoise-1 cursor-pointer w-6";
 
 export const FooterData = [
   {
-    title: "E-mail: Claudia@email.com",
+    title: "R. Conde do Pinhal, 196 - Centro, Jaú - SP, 17201-040",
     cName: defaultCname,
+    icon: <LocationIcon />,
   },
 
   {
-    title: "Telefone: (14) 99999-8888",
+    title: "E-mail: claudia.iroma@hotmail.com",
     cName: defaultCname,
+    icon: <EmailIcon />,
   },
 
   {
-    title: "Me siga nas redes sociais!",
+    title: "Telefone: (14) 99703-0269",
     cName: defaultCname,
+    icon: <PhoneIcon />,
+    icon2: <WhatsappIcon />,
   },
+
+  // {
+  //   title: "Me siga nas redes sociais!",
+  //   cName: defaultCname,
+  // },
 
   {
     socialLinks: [
