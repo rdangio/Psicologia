b3b9a0bdfeaeb3bf79e5b5c3980c439d3baf3dfa
Rico <ricojau@hotmail.com>
2024-12-13 13:11:34 -0300

    codigo do componemte para montar a página saiba mais

--- src/components/ActuationExplain/ActuationExplain.jsx
+++ src/components/ActuationExplain/ActuationExplain.jsx
@@ -6,16 +6,18 @@ function ActuationExplain() {
     <CentralizedContainer className="mt-10">
       <div className="">
         <div className="flex flex-row">
-          
           {/* essa é a parte do capitulo */}
-          <div className="flex h-dvh w-[30%] bg-yellow-300 text-left">
-            <ul>
+          <div className="0 flex h-dvh w-[30%] bg-c-turquoise-1 text-left">
+            <ul className="w-full">
               {dataActuationExplain.map((actuation) => {
                 return (
-                  <li key={actuation.id}>
+                  <li className="" key={actuation.id}>
                     {/* Mapeia todos os elementos do array actuation.capitulo */}
-                    {actuation.capitulo.map((item, index) => (
-                      <div key={index}>{item}</div>
+                    {actuation.chapter.map((item, index) => (
+                      <div className="w-full" key={index}>
+                        {item}
+                        <div className="mb-3 border-b-2 border-c-turquoise-6 pt-1"></div>
+                      </div>
                     ))}
                   </li>
                 );
@@ -23,10 +25,23 @@ function ActuationExplain() {
             </ul>
           </div>
 
-
-          <div className="w-[70%] bg-green-500 text-center">
-            
-            
+          <div className="w-[70%] border-l-2 border-x-c-turquoise-6 bg-c-turquoise-1">
+            <h1 className="p-5 text-left font-roboto-bold">
+              {dataActuationExplain[0].title}
+            </h1>
+            <ul>
+              {dataActuationExplain.map((actuation) => (
+                <li className="pl-5" key={actuation.id}>
+                  {actuation.chapter.map((chapterItem, index) => (
+                    <div key={index}>
+                      <h2 className="p-1 font-bold">{chapterItem}</h2>
+                      {actuation.text[index] && (<h3 className="indent-8 hyphens-auto text-justify pb-5"
+                        dangerouslySetInnerHTML={{ __html: actuation.text[index]}}/>)}
+                    </div>
+                  ))}
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
         ;
--- src/components/ActuationExplain/DataActuationExplain.jsx
+++ src/components/ActuationExplain/DataActuationExplain.jsx
@@ -1,49 +1,45 @@
 export const dataActuationExplain = [
-  
   {
     id: "1",
     title: "Transtornos de Ansiedade",
-    capitulo: ["capitulo1", "capitulo2", "capitulo3", "capitulo4", "capitulo5", "capitulo6"],
-    text1: "texto1",
-    text2: "texto2",
-    text3: "texto3",
-    text4: "texto4",
-    text5: "texto5",
-    text6: "texto6",
+    chapter: [
+      "O que é ansiedade? ",
+      "Quais são os sinais e sintomas de ansiedade?",
+      "Quais são os fatores de risco para a ansiedade? ",
+      "Como a ansiedade é tratada?",
+    ],
+    text: [
+      `A ansiedade ocasional é uma parte normal da vida. Muitas pessoas se preocupam com questões como saúde, dinheiro ou problemas familiares. Mas os transtornos de ansiedade envolvem mais do que uma preocupação ou medo temporários. Para pessoas com transtorno de ansiedade, a ansiedade não vai embora e pode piorar com o tempo. Os sintomas podem interferir nas atividades diárias, como desempenho no trabalho, nos estudos e nos relacionamentos. Existem vários tipos de transtornos de ansiedade, incluindo transtorno de ansiedade generalizada, transtorno de pânico, transtorno de ansiedade social e vários transtornos relacionados a fobias`,
+
+      `Os sinais mais comuns são:<br/>
+      •	Sentir-se inquieto, nervoso ou tenso<br/>
+      •	Cansar-se facilmente<br/>
+      •	Dificuldade em concentrar-se<br/>
+      •	Irritabilidade<br/>
+      •	Dores de cabeça, musculares, de estômago ou dores inexplicáveis<br/>
+      •	Dificuldade em controlar os sentimentos de preocupação<br/>
+      •	Problemas de sono, como dificuldade em adormecer ou manter-se dormindo<br/>
+      `,
+
+      `Os pesquisadores estão descobrindo que fatores genéticos e ambientais contribuem para o risco de desenvolver um transtorno de ansiedade.
+      Os fatores de risco para cada tipo de transtorno de ansiedade variam. No entanto, alguns fatores de risco gerais incluem:<br/>
+      •	Timidez ou sensação de aflição ou nervosismo em novas situações na infância<br/>
+      •	Exposição a eventos de vida estressantes e negativos ou eventos ambientais<br/>
+      •	Um histórico de ansiedade ou outros transtornos mentais em parentes biológicos<br/>
+      Os sintomas de ansiedade podem ser produzidos ou agravados por:<br/>
+      •	Algumas condições de saúde física, como problemas de tireoide ou arritmia cardíaca<br/>
+      •	Cafeína ou outras substâncias/medicamentos<br/>
+      Se você acha que pode ter um transtorno de ansiedade, fazer um exame físico com um profissional de saúde pode ajudá-lo a diagnosticar seus sintomas e encontrar o tratamento certo.
+      `,
+
+      `Os transtornos de ansiedade são geralmente tratados com psicoterapia, medicação ou ambos. Existem muitas maneiras de tratar a ansiedade, e você deve trabalhar com um profissional de saúde para escolher o melhor tratamento para você.<br/>
+      • Psicoterapia<br/>
+      • Terapia cognitivo-comportamental (TCC)<br/> 
+      • Terapia de aceitação e compromisso (ACT)<br/> 
+      • Antidepressivos<br/>
+      • Medicação<br/>
+      • Medicamentos anti-ansiedade<br/>
+      `,
+    ],
   },
-
-  // {
-  //   id: "2",
-  //   title: "Relacionamentos",
-  //   iten1: "capitulo1",
-  //   iten2: "capitulo2",
-  //   iten3: "capitulo3",
-  //   iten4: "capitulo4",
-  //   iten5: "capitulo5",
-  //   iten6: "capitulo6",
-  //   text1: "texto1",
-  //   text2: "texto2",
-  //   text3: "texto3",
-  //   text4: "texto4",
-  //   text5: "texto5",
-  //   text6: "texto6",
-  // },
-
-  // {
-  //   id: "3",
-  //   title: "Depressão",
-  //   iten1: "capitulo1",
-  //   iten2: "capitulo2",
-  //   iten3: "capitulo3",
-  //   iten4: "capitulo4",
-  //   iten5: "capitulo5",
-  //   iten6: "capitulo6",
-  //   text1: "texto1",
-  //   text2: "texto2",
-  //   text3: "texto3",
-  //   text4: "texto4",
-  //   text5: "texto5",
-  //   text6: "texto6",
-  // },
-
 ];
