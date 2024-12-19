ded6c215b992fb61269a5bc5637f14a3bd24b3a3
deivideguilherme <deivide.jau@gmail.com>
2024-12-19 14:09:49 -0300

    chore: Alterado rota da página de contato, removido a barra do path

--- src/routes.jsx
+++ src/routes.jsx
@@ -14,7 +14,7 @@ const AppRoutes = () => {
         <Route path="0" element={<AreasDeAtuacao numero={1} />}></Route>
         <Route path="1" element={<AreasDeAtuacao numero={2} />}></Route>
         <Route path="2" element={<AreasDeAtuacao numero={3} />}></Route>
-        <Route path="/contato" element={<Contact />}></Route>
+        <Route path="contato" element={<Contact />}></Route>
       </Routes>
     </BrowserRouter>
   );
