7bdb2d3f348f24df21ecd2e636ac74ababeb8d49
deivideguilherme <deivide.jau@gmail.com>
2024-12-18 22:53:05 -0300

    chore: Ajustes de cores formulário

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -63,11 +63,11 @@ function Form() {
             type="text"
             onChange={(e) => setName(capitalizeWords(e.target.value))}
             value={name}
-            className="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-0 py-2.5 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-blue-500"
+            className="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-0 py-2.5 text-sm text-gray-900 focus:border-c-turquoise-6 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-c-turquoise-5"
             placeholder=" "
             required
           />
-          <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm tracking-wider text-gray-500 duration-300 placeholder:text-fs-h6 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:start-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600 rtl:peer-focus:translate-x-1/4 dark:text-gray-400 peer-focus:dark:text-blue-500">
+          <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm tracking-wider text-gray-500 duration-300 placeholder:text-fs-h6 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:start-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-c-turquoise-4 rtl:peer-focus:translate-x-1/4 dark:text-gray-400 peer-focus:dark:text-c-turquoise-3">
             Seu nome
           </label>
         </div>
@@ -78,11 +78,11 @@ function Form() {
             type="text"
             onChange={(e) => setEmail(e.target.value)}
             value={email}
-            className="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-0 py-2.5 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-blue-500"
+            className="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-0 py-2.5 text-sm text-gray-900 focus:border-c-turquoise-6 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-c-turquoise-5"
             placeholder=" "
             required
           />
-          <label className="cap absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm tracking-wider text-gray-500 duration-300 placeholder:text-fs-h6 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:start-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4 dark:text-gray-400 peer-focus:dark:text-blue-500">
+          <label className="cap absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm tracking-wider text-gray-500 duration-300 placeholder:text-fs-h6 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:start-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-c-turquoise-4 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4 dark:text-gray-400 peer-focus:dark:text-c-turquoise-3">
             Seu e-mail
           </label>
         </div>
@@ -92,11 +92,11 @@ function Form() {
           <textarea
             onChange={(e) => setMessage(e.target.value)}
             value={message}
-            className="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-0 py-2.5 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-blue-500"
+            className="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-0 py-2.5 text-sm text-gray-900 focus:border-c-turquoise-6 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-c-turquoise-5"
             placeholder=" "
             required
           />
-          <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm tracking-wider text-gray-500 duration-300 placeholder:text-fs-h6 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:start-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600 rtl:peer-focus:translate-x-1/4 dark:text-gray-400 peer-focus:dark:text-blue-500">
+          <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm tracking-wider text-gray-500 duration-300 placeholder:text-fs-h6 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:start-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-c-turquoise-4 rtl:peer-focus:translate-x-1/4 dark:text-gray-400 peer-focus:dark:text-c-turquoise-3">
             Sua mensagem
           </label>
         </div>
